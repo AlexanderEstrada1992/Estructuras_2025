225d6979caf5f0011c9e9d7b0ee0694d1fd5b3da
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-06-13 18:55:45 -0500

    Tarea Semana 3; Registro de un estudiante - clases, arrays

--- EstructurasApp/Program.cs
+++ EstructurasApp/Program.cs
@@ -1,25 +1,31 @@
-﻿System.Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
+﻿// Mostrar encabezado institucional
+System.Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
 System.Console.WriteLine("=============================");
 System.Console.WriteLine("REGISTRO DE ESTUDIANTE");
 System.Console.WriteLine();
 
+// Declarar e inicializar los datos del estudiante
 int id = 1;
 string nombres = "Juan";
 string apellidos = "Pérez";
 string direccion = "Calle Amazonas y Río Napo";
 
+// Crear un array de 3 teléfonos del estudiante
 string[] telefonos = new string[3];
 telefonos[0] = "0987654321";
 telefonos[1] = "022345678";
 telefonos[2] = "0934567890";
 
+// Crear un objeto de la clase Estudiante usando los datos
 Estudiante estudiante1 = new Estudiante(id, nombres, apellidos, direccion, telefonos);
 
+// Mostrar los datos del estudiante usando los métodos de la clase
 System.Console.WriteLine("ID: " + estudiante1.getId());
 System.Console.WriteLine("Nombres: " + estudiante1.getNombres());
 System.Console.WriteLine("Apellidos: " + estudiante1.getApellidos());
 System.Console.WriteLine("Dirección: " + estudiante1.getDireccion());
 
+// Mostrar los teléfonos uno por uno
 System.Console.WriteLine("Teléfonos:");
 string[] telfs = estudiante1.getTelefonos();
 System.Console.WriteLine("- " + telfs[0]);
--- EstructurasApp/Semana3.cs
+++ EstructurasApp/Semana3.cs
@@ -1,11 +1,14 @@
+// Clase Estudiante para almacenar la información personal de un estudiante
 public class Estudiante
 {
+    // Atributos privados del estudiante
     private int id;
     private string nombres;
     private string apellidos;
     private string direccion;
-    private string[] telefonos;
+    private string[] telefonos; // Array para guardar 3 teléfonos
 
+    // Constructor que recibe los datos y los asigna a los atributos
     public Estudiante(int _id, string _nombres, string _apellidos, string _direccion, string[] _telefonos)
     {
         id = _id;
@@ -15,26 +18,31 @@ public class Estudiante
         telefonos = _telefonos;
     }
 
+    // Método para obtener el ID
     public int getId()
     {
         return id;
     }
 
+    // Método para obtener los nombres
     public string getNombres()
     {
         return nombres;
     }
 
+    // Método para obtener los apellidos
     public string getApellidos()
     {
         return apellidos;
     }
 
+    // Método para obtener la dirección
     public string getDireccion()
     {
         return direccion;
     }
 
+    // Método para obtener el array de teléfonos
     public string[] getTelefonos()
     {
         return telefonos;
