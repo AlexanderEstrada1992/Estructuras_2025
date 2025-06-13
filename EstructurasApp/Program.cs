07a66543de6125072f92fe6eaa351bf7ebe933c4
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-06-13 16:11:51 -0500

    Tarea Semana 2 ; Función área y Perímetro de un cuadrado

--- EstructurasApp/Program.cs
+++ EstructurasApp/Program.cs
@@ -1,2 +1,17 @@
 ﻿Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
 Console.WriteLine("=============================");
+Console.WriteLine();
+
+System.Console.WriteLine("el perímtro de un cuadrado de lado 5 es " + perímetroCuadrado(5));
+System.Console.WriteLine("el área de un cuadrado de lado 5 es " + areaCuadrado(5));
+
+
+// Función para calcular el perímetro de un cuadrado o rectángulo
+double perímetroCuadrado(double lado)
+{
+    return lado + lado + lado + lado;
+}
+double areaCuadrado(double lado)
+{
+    return lado * lado;
+}
