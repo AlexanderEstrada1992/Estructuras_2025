1172e8b724229c975db4955bf3f83eccddae9c7f
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-06-21 19:43:49 -0500

    Descripción de las propiedades

--- EstructurasApp/clsContacto.cs
+++ EstructurasApp/clsContacto.cs
@@ -1,2 +1,8 @@
 // Definición de la clase Contacto que representa un registro
 public class Contacto
+{
+    // Propiedades del contacto
+    public string Nombre { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+}
