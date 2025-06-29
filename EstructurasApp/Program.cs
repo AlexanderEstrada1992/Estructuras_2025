5bd9b6633bfc4fa932d7609bfee75e5270bfa9f9
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-06-28 22:26:45 -0500

    Carátula del ejercicio 1

--- EstructurasApp/Program.cs
+++ EstructurasApp/Program.cs
@@ -2,11 +2,49 @@
 System.Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
 System.Console.WriteLine("=============================");
 System.Console.WriteLine();
-System.Console.WriteLine("  Listas");
+System.Console.WriteLine("  Ejercicio 1 - Asignaturas");
 System.Console.WriteLine("=============================");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// System.Console.WriteLine("  Listas");
+// System.Console.WriteLine("=============================");
+
+// // Crear una colección de strings llamada nombres
+// List<string> nombres = ["Ana", "Luis", "Rosita"];
+
+// // Añadir elementos a la lista
+// nombres.Add("Romina");
+// nombres.Add("Willy");
+// nombres.Add("Ana");
+
+// // Eliminar elemento de la lista
+// nombres.Remove("Ana");
+
+// // Ordenar una lista
+// nombres.Sort();
+
+// // Imprimir una lista
+// foreach (string nombre in nombres)
+// {
+//     System.Console.WriteLine(nombre.ToUpper());
+// }
+
+// // Cuenta los elementos de la lista
+// System.Console.WriteLine($"La lista tiene {nombres.Count} elementos");
 
 
 
