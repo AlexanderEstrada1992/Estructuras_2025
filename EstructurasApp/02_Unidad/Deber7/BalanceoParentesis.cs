dfde552a853361537a27c06a4ec17294221646ba
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-13 17:05:10 -0500

    Recorrer cada caracter

--- EstructurasApp/02_Unidad/Deber7/BalanceoParentesis.cs
+++ EstructurasApp/02_Unidad/Deber7/BalanceoParentesis.cs
@@ -5,3 +5,23 @@ public class BalanceoParentesis
     {
         Stack<char> pila = new Stack<char>(); // Pila de caracteres
         bool balanceado = true;
+
+        // Recorremos cada caracter de la expresión
+        foreach (char caracter in expresion)
+        {
+            // Si es un paréntesis de apertura, lo apilamos
+            if (caracter == '(' || caracter == '{' || caracter == '[')
+            {
+                pila.Push(caracter);
+            }
+            // Si es un paréntesis de cierre
+            else if (caracter == ')' || caracter == '}' || caracter == ']')
+            {
+                // Si la pila está vacía, hay un error
+                if (pila.Count == 0)
+                {
+                    balanceado = false;
+                    break;
+                }
+
+                char tope = pila.Pop(); // Extraemos el último abierto
