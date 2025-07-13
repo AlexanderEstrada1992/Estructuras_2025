73816747a5c0ecfe2f4cc0dad2280f5cad009a7c
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-13 16:33:35 -0500

    Creación de archivo para paréntesis balanceados

--- EstructurasApp/Program.cs
+++ EstructurasApp/Program.cs
@@ -2,16 +2,8 @@
 Console.WriteLine("=============================");
 Console.WriteLine();
 
+pilas.run();
 
-Stack<string> estudiantes = new Stack<string>();
-
-estudiantes.Push("Ana");
-estudiantes.Push("Carlos");
-System.Console.WriteLine("Ingrese un nombre");
-estudiantes.Push(Console.ReadLine());
-estudiantes.Push("Juan");
-
-pilas.ImprimirPila(estudiantes);
 
 
 
@@ -20,7 +12,7 @@ pilas.ImprimirPila(estudiantes);
 //EjemploAgenda.run();
 //Agenda.run();
 //Deber6.run();
-pilas.run():
+
 
 
 
