d06dbcdd79bfe88660b8b8354d57269de348b3f4
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-13 17:19:10 -0500

    Solución al problema de las Torres de Hanoi

--- EstructurasApp/Program.cs
+++ EstructurasApp/Program.cs
@@ -7,10 +7,15 @@
         Console.WriteLine("=============================");
         Console.WriteLine();
 
-         // Verificación de paréntesis balanceados
+        // Verificación de paréntesis balanceados
         Console.WriteLine("\nVerificación de paréntesis:");
         string expresion = "{7 + (8 * 5) - [(9 - 7) + (4 + 1)]}";
         BalanceoParentesis.VerificarBalanceo(expresion);
+        
+        // Solución al problema de las Torres de Hanoi
+        Console.WriteLine("\nResolución Torres de Hanoi:");
+        int numDiscos = 3; // Número de discos a mover
+        TorresDeHanoi.ResolverHanoi(numDiscos, "A", "C", "B");
     }
 }
 
