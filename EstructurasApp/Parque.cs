ea7a4a561510500b572e3a6c236dff07eda2b27a
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-19 23:19:24 -0500

    Muestro el total de personas

--- EstructurasApp/Parque.cs
+++ EstructurasApp/Parque.cs
@@ -4,8 +4,15 @@ public class Parque
     {
         // Creamos una cola vacía para almacenar los nombres de las personas
         Queue<string> personas = new Queue<string>();
-        
+
         // Agregamos 30 personas a la cola en orden de llegada
         for (int i = 1; i <= 30; i++)
+        {
+            string nombrePersona = "Persona " + i;
+            personas.Enqueue(nombrePersona); // Se agrega a la cola
+        }
+
+         // Mostramos el total de personas en la cola
+        System.Console.WriteLine("Total de personas en la fila: " + personas.Count());
      }
 }
