d04b6b0bc0f42acb26a0ae93ee966777b7261ee4
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-19 23:21:46 -0500

    Asignación de asientos en orden de llegada

--- EstructurasApp/Parque.cs
+++ EstructurasApp/Parque.cs
@@ -12,7 +12,16 @@ public class Parque
             personas.Enqueue(nombrePersona); // Se agrega a la cola
         }
 
-         // Mostramos el total de personas en la cola
+        // Mostramos el total de personas en la cola
         System.Console.WriteLine("Total de personas en la fila: " + personas.Count());
+        
+        // Empezamos a asignar asientos en orden de llegada
+        int asiento = 1;
+        while (personas.Count() > 0)
+        {
+            string persona = personas.Dequeue(); // Se atiende a la primera persona de la cola
+            System.Console.WriteLine(persona + " ha ocupado el asiento número " + asiento);
+            asiento++; // Pasamos al siguiente asiento
+        }
      }
 }
