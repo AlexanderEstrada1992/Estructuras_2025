fab10ed81ca9d0594f4cfb11cbfd89155adcf682
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-19 23:18:19 -0500

    Agrego 30 personas en orden de llegada

--- EstructurasApp/Parque.cs
+++ EstructurasApp/Parque.cs
@@ -1,8 +1,11 @@
 public class Parque
 {
     public static void run()
-     {
+    {
         // Creamos una cola vacía para almacenar los nombres de las personas
         Queue<string> personas = new Queue<string>();
+        
+        // Agregamos 30 personas a la cola en orden de llegada
+        for (int i = 1; i <= 30; i++)
      }
 }
