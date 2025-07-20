9f293bc30cebccbcd2b840af58457b6d6222bd8a
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-07-19 23:17:35 -0500

    Creación de una cola vacía

--- EstructurasApp/Parque.cs
+++ EstructurasApp/Parque.cs
@@ -1,4 +1,8 @@
 public class Parque
 {
-    
+    public static void run()
+     {
+        // Creamos una cola vacía para almacenar los nombres de las personas
+        Queue<string> personas = new Queue<string>();
+     }
 }
