01486bc89e5d337d5f2b1446345fb9c597600f7f
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-08-24 16:41:49 -0500

    Creación de conjunto ficticio de 500 ciudadanos

--- EstructurasApp/03 -Unidad/Semana10.cs
+++ EstructurasApp/03 -Unidad/Semana10.cs
@@ -3,6 +3,13 @@ public class vacunas
     public static void run()
     {
         Random random = new Random();
+
+         // Conjunto de 500 ciudadanos ficticios
+        HashSet<string> ciudadanos = new HashSet<string>();
+        for (int i = 1; i <= 500; i++)
+        {
+            ciudadanos.Add("persona" + i);
+        }
     }
 
 }
