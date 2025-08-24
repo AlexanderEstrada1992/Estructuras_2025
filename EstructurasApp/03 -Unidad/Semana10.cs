918448a923b9f918207b9b37d196cd652306eab8
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-08-24 16:43:01 -0500

    Conjunto de 75 ciudadanos vacunados con Pfizer

--- EstructurasApp/03 -Unidad/Semana10.cs
+++ EstructurasApp/03 -Unidad/Semana10.cs
@@ -4,12 +4,21 @@ public class vacunas
     {
         Random random = new Random();
 
-         // Conjunto de 500 ciudadanos ficticios
+        // Conjunto de 500 ciudadanos ficticios
         HashSet<string> ciudadanos = new HashSet<string>();
         for (int i = 1; i <= 500; i++)
         {
             ciudadanos.Add("persona" + i);
         }
+
+        // Conjunto de vacunados con Pfizer (75 ciudadanos)
+        HashSet<string> pfizer = new HashSet<string>();
+        while (pfizer.Count < 75)
+        {
+            pfizer.Add("persona" + random.Next(1, 501));
+        }
+        
+        
     }
 
 }
