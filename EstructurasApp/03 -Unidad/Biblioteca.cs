b5022e18218129ca3dabbdaa678e9cbad3ef87c6
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-09-06 22:29:58 -0500

    Creación de conjunto y mapa

--- EstructurasApp/03 -Unidad/Biblioteca.cs
+++ EstructurasApp/03 -Unidad/Biblioteca.cs
@@ -2,7 +2,13 @@ public class Biblioteca
 {
     public static void run()
     {
+        //Conjunto y mapa
+        string[] titulos = new string[50];  
+        string[] autores = new string[50];  
+        int[] anios = new int[50];          
+        int contador = 0;                   
 
+        int opcion = -1;
     }
 
 }
