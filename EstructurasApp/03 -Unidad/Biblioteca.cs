c8499b61a3cd78fbd0b509eb9bfdd2e1db0ad0e7
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-09-06 22:31:53 -0500

    Creación del menú interactivo

--- EstructurasApp/03 -Unidad/Biblioteca.cs
+++ EstructurasApp/03 -Unidad/Biblioteca.cs
@@ -2,13 +2,27 @@ public class Biblioteca
 {
     public static void run()
     {
-        //Conjunto y mapa
-        string[] titulos = new string[50];  
-        string[] autores = new string[50];  
-        int[] anios = new int[50];          
-        int contador = 0;                   
+        // Conjunto y mapa
+        string[] titulos = new string[50];
+        string[] autores = new string[50];
+        int[] anios = new int[50];
+        int contador = 0;
 
         int opcion = -1;
+
+        // Menú Interactivo
+        while (opcion != 0)
+        {
+            Console.WriteLine("\n==================== MENÚ ====================");
+            Console.WriteLine("1. Registrar un libro");
+            Console.WriteLine("2. Mostrar todos los libros");
+            Console.WriteLine("3. Buscar libro por título");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+            
+             
+        }
     }
 
 }
