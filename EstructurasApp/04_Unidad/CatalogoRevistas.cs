5eb72880420d090beb2fde0414e5aade5abc09c1
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-09-14 13:07:50 -0500

    Arreglo  para almacenar los títulos de las revistas

--- EstructurasApp/04_Unidad/CatalogoRevistas.cs
+++ EstructurasApp/04_Unidad/CatalogoRevistas.cs
@@ -1,5 +1,7 @@
 public class CatalogoRevistas
 {
+    // Arreglo estático para almacenar los títulos de las revistas
+    static string[] revistas = new string[10];
     public static void run()
     {
 
