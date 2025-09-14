1648d9b6039dd138dfc775c03d8f4c416a9ba11a
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-09-14 13:09:12 -0500

    Creación del catálogo

--- EstructurasApp/04_Unidad/CatalogoRevistas.cs
+++ EstructurasApp/04_Unidad/CatalogoRevistas.cs
@@ -4,6 +4,16 @@ public class CatalogoRevistas
     static string[] revistas = new string[10];
     public static void run()
     {
-
+        // Creación del catálogo
+        revistas[0] = "Revista Ciencia Hoy";
+        revistas[1] = "Tecnología Actual";
+        revistas[2] = "Mundo Salud";
+        revistas[3] = "Viajes y Aventuras";
+        revistas[4] = "Cocina Fácil";
+        revistas[5] = "Deportes al Día";
+        revistas[6] = "Historia y Cultura";
+        revistas[7] = "Innovación Empresarial";
+        revistas[8] = "Arte y Diseño";
+        revistas[9] = "Educación Global";
     }
 }
