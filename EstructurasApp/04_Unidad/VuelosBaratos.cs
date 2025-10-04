649c285589f3fb1eaac9fa707240b6b720f5a166
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-10-04 17:48:18 -0500

    Arreglo estático para almacenar vuelos

--- EstructurasApp/04_Unidad/VuelosBaratos.cs
+++ EstructurasApp/04_Unidad/VuelosBaratos.cs
@@ -1,5 +1,9 @@
 public class VuelosBaratos
 {
+    // Arreglo estático para almacenar vuelos
+    static string[] origenes = new string[6];
+    static string[] destinos = new string[6];
+    static int[] precios = new int[6];
     public static void run()
     {
 
