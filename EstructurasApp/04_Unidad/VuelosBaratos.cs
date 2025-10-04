8eaff1ce94b4f1d19188afe91c4fbcf994639a29
ALEXANDER ESTRADA <ja.estradas@uea.edu.ec>
2025-10-04 17:49:40 -0500

    Creación de la base de datos ficticia

--- EstructurasApp/04_Unidad/VuelosBaratos.cs
+++ EstructurasApp/04_Unidad/VuelosBaratos.cs
@@ -4,9 +4,18 @@ public class VuelosBaratos
     static string[] origenes = new string[6];
     static string[] destinos = new string[6];
     static int[] precios = new int[6];
+
     public static void run()
     {
+        // Creación de la base de datos ficticia
+        origenes[0] = "Quito"; destinos[0] = "Guayaquil"; precios[0] = 60;
+        origenes[1] = "Quito"; destinos[1] = "Cuenca"; precios[1] = 50;
+        origenes[2] = "Quito"; destinos[2] = "Manta"; precios[2] = 45;
+        origenes[3] = "Guayaquil"; destinos[3] = "Cuenca"; precios[3] = 40;
+        origenes[4] = "Guayaquil"; destinos[4] = "Quito"; precios[4] = 55;
+        origenes[5] = "Cuenca"; destinos[5] = "Manta"; precios[5] = 70;
 
+        
     }
 
 }
